b4e952093d145f5a05f299c4ac8417acd9ba6e6c
Sergio <sergiokv13@gmail.com>
2017-08-11 15:08:31 -0400

    c

--- app/assets/javascripts/channels/refrescar.js
+++ app/assets/javascripts/channels/refrescar.js
@@ -1,7 +1,13 @@
 (function() {
-  
+  App.refrescar = App.cable.subscriptions.create("RefrescarChannel", {
+    connected: function() {},
+    disconnected: function() {},
+    received: function(data) {
+      if (true) {
         return location.reload();
-     
+      }
+    }
+  });
 
 }).call(this);
 
