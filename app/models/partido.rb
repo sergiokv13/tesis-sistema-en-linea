06f5f15ae96bb667b9e8f89c883e467d59d22274
Sergio <sergiokv13@gmail.com>
2017-08-12 14:19:54 -0400

    nueva direccion Blockchain

--- app/models/partido.rb
+++ app/models/partido.rb
@@ -1,4 +1,15 @@
 class Partido < ApplicationRecord
   belongs_to :votacion
   has_many :opcions
+
+  before_create :crear_direccion
+
+    def crear_direccion
+      	cmd = "multichain-cli effex-chain getnewaddress"
+		#Saves the result of command in res
+		res = %x[#{cmd}]
+		#Gets the new address
+		self.direccion =  res.split('}')[1][2..-2]
+    end
+
 end
